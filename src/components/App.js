7b325c247c79e0df8c8b9af8d4198c4f1d024cc7
suraj-mandal <suraj.mandal738@gmail.com>
2023-09-11 23:56:36 +0530

    Added template for creating a new react project

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+// the App class has bunch of properties inherited from the Component class
+class App extends Component {
+    render() {
+        return <div>
+            React App
+        </div>
+   }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import App from "./components/App";
+
+import "./index.css";
+
+// import AppWithHeader from "./components/App";
+// import JokesWithHeader from "./components/Jokes";
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App />);
+
